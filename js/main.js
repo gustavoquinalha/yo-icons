d69d1fdb43b74cc24a47a808db51823e1243837e
gustavoquinalha <gustavo_quinalha@hotmail.com>
2017-05-10 17:38:55 -0300

    -q

---
+++ js/main.js
@@ -0,0 +1,48 @@
+  var iconApp = angular.module('iconApp', ['ngRoute']);
+  iconApp.config(function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'icons-list.html',
+        controller: 'IconsListCtrl'
+      }).
+      when('/:countryName', {
+        templateUrl: 'icon-detail.html',
+        controller: 'IconsDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  });
+  iconApp.factory('icons', function($http){
+    function getData(callback){
+      $http({
+        method: 'GET',
+        url: 'icons.json',
+        cache: true
+      }).success(callback);
+    }
+    return {
+      list: getData,
+      find: function(name, callback){
+        getData(function(data) {
+          var country = data.filter(function(entry){
+            return entry.name === name;
+          })[0];
+          callback(country);
+        });
+      }
+    };
+  });
+  iconApp.controller('IconsListCtrl', function ($scope, icons){
+    icons.list(function(icons) {
+      $scope.icons = icons;
+    });
+  });
+  iconApp.controller('IconsDetailCtrl', function ($scope, $routeParams, icons){
+    icons.find($routeParams.countryName, function(country) {
+      $scope.country = country;
+    });
+  });
+  iconApp.filter('encodeURI', function(){
+    return window.encodeURI;
+  });
